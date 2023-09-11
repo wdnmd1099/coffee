4498e3841020f01a9eed56147cdd41eb15f586e7
ggbond <wdnmd@gmail.com>
2023-09-11 12:02:47 +0800

    更新保存8(完成选配菜单)

--- lib/component/optionCardList.dart
+++ lib/component/optionCardList.dart
@@ -4,41 +4,48 @@ import 'package:coffee/stateManage/stateManage.dart';
 
 class OptionCardList extends StatefulWidget {
   final List optionList;
+  List<Map<String, dynamic>> eee = [
+    {
+      '状态': ['冰(推荐)', '比较烫', '温'],
+    },
+    {
+      '茶底': [
+        '绿妍(推荐)',
+        '百岁山(不推荐)',
+        '去茶底',
+      ],
+    },
+    {
+      '糖量': [
+        '正常糖',
+        '少糖',
+        '少少糖',
+        '多糖',
+      ],
+    },
+  ];
 
-  const OptionCardList({super.key, required this.optionList});
+  OptionCardList({super.key, required this.optionList});
 
   @override
-  State<OptionCardList> createState() => _OptionCardListState();
+  State<OptionCardList> createState() => _OptionCardListState(eee,optionList);
 }
 
 class _OptionCardListState extends State<OptionCardList> {
+  Map selectedValues = {};
+  Map changedSelectedValues = {};
+  int drinkState = 0;
+  _OptionCardListState(List<Map<String, dynamic>> eee, List optionList) {
+    for(int i=0;i<optionList.length;i++){
+      selectedValues[optionList[i].keys.first] = 0;
+    }
+    for(int i=0;i<eee.length;i++){
+      changedSelectedValues[eee[i].keys.first] = 0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    double maxHeight = MediaQuery.of(context).size.height;
-    double maxWidth = MediaQuery.of(context).size.width;
-    List<Map<String, dynamic>> eee = [
-      {
-        '状态': ['冰(推荐)', '比较烫', '温'],
-        '选中':0,
-      },
-      {
-        '茶底': [
-          '绿妍(推荐)',
-          '百岁山(不推荐)',
-          '去茶底',
-        ],
-        '选中':0,
-      },
-      {
-        '糖量': [
-          '正常糖',
-          '少糖',
-          '少少糖',
-          '多糖',
-        ],
-        '选中':0,
-      },
-    ];
     final changeMenuOptions = Provider.of<ChangeMenuOptions>(context);
     return changeMenuOptions.isHot == false
         ? ListView.builder(
@@ -79,19 +86,29 @@ class _OptionCardListState extends State<OptionCardList> {
                             runSpacing: 8.0,
                             // 换行时行与行之间的间距
                             children: [
-                              for (int i = 0;
-                                  i < item[item.keys.first].length;
-                                  i++)
+                              for (int i = 0; i < item[item.keys.first].length; i++)
                                 ElevatedButton(
                                   onPressed: () => {
-                                    test(item[item.keys.first][i],
-                                        changeMenuOptions)
+                                    setState(() {
+                                      test(item[item.keys.first]?[i], changeMenuOptions);
+                                      if(item.keys.first != '状态'){
+                                        selectedValues[item.keys.first] = i;
+                                      }
+                                      else if((item.keys.first == '状态')){
+                                        selectedValues[item.keys.first] = i;
+                                        changedSelectedValues[item.keys.first] = i;
+                                      }
+                                      print(selectedValues);
+                                      print(changedSelectedValues);
+                                    })
                                   },
                                   style: ElevatedButton.styleFrom(
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(0),
                                     ),
-                                    backgroundColor: Colors.black,
+                                    backgroundColor: selectedValues[item.keys.first] == i
+                                        ? Colors.black
+                                        : Colors.grey,
                                   ),
                                   child: Container(
                                     // color: Colors.black,
@@ -109,15 +126,14 @@ class _OptionCardListState extends State<OptionCardList> {
             },
           )
         : ListView.builder(
-            // shrinkWrap: true,
-            //       physics: NeverScrollableScrollPhysics(),
-            itemCount: eee.length, //数组长度
+            itemCount: widget.eee.length, //数组长度
             itemBuilder: (BuildContext context, int index1) {
-              var item1 = eee[index1]; //index是当前元素的下标，这样就指当前元素
-              List<String> keys = item1[item1.keys.first].toList();
-              String selectedKeys = keys[0];
-              print(keys[0]);
-              if(item1[item1.keys.first] == null || item1[item1.keys.first]?.length == 0){
+              var item1 = widget.eee[index1]; //index是当前元素的下标，这样就指当前元素
+              // changedSelectedValues[item1.keys.first] = 0;
+              // print(changedSelectedValues);
+              // print(changedSelectedValues);
+              if (item1[item1.keys.first] == null ||
+                  item1[item1.keys.first]?.length == 0) {
                 print('更新的数组内容为空');
                 throw '更新的数组内容为空';
               }
@@ -149,19 +165,34 @@ class _OptionCardListState extends State<OptionCardList> {
                             runSpacing: 8.0,
                             // 换行时行与行之间的间距
                             children: [
-                              for (int i = 0; i < item1[item1.keys.first]!.length; i++)
+                              for (int i = 0;
+                                  i < item1[item1.keys.first]!.length; i++)
                                 ElevatedButton(
                                   onPressed: () => {
-                                     test(item1[item1.keys.first]?[i], changeMenuOptions)
+                                    setState(() {
+                                      test(item1[item1.keys.first]?[i], changeMenuOptions);
+                                      if(item1.keys.first != '状态'){
+                                        changedSelectedValues[item1.keys.first] = i;
+                                      }
+                                      else if((item1.keys.first == '状态')){
+                                        selectedValues[item1.keys.first] = i;
+                                        changedSelectedValues[item1.keys.first] = i;
+                                      }
+                                      print(selectedValues);
+                                      print(changedSelectedValues);
+                                    })
                                   },
                                   style: ElevatedButton.styleFrom(
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(0),
                                     ),
-                                    backgroundColor: selectedKeys!=null?Colors.black:Colors.grey,
+                                    backgroundColor: changedSelectedValues[item1.keys.first] == i
+                                        ? Colors.black
+                                        : Colors.grey,
                                   ),
                                   child: Container(
-                                    child: Text('${item1[item1.keys.first]?[i]}'),
+                                    child:
+                                        Text('${item1[item1.keys.first]?[i]}'),
                                   ),
                                 ),
                             ],
@@ -177,21 +208,13 @@ class _OptionCardListState extends State<OptionCardList> {
   }
 }
 
-int test2 = 1;
-
-String test(item233, _changeMenuOptions, ) {
+test(item233, _changeMenuOptions) {
+  // print(item233);
   if (item233 == '温' || item233 == '比较烫') {
     // print('是温的');
     _changeMenuOptions.Hot();
-  } else if(item233 == '冰(推荐)'){
+  } else if (item233 == '冰(推荐)') {
     // print('不是温的');
     _changeMenuOptions.Cold();
   }
-  else if(item233 == '少少糖'){
-    print('少少糖');
-    test2 = 2;
-    print(test2);
-  }
-
-  return item233;
 }
